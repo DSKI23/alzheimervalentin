1be374413b1526250a1f1efd516cb18ee6e707f9
Klagge, Philipp <philipp.klagge@wika.com>
2025-02-27 14:56:06 +0100

    trying to automate model training on push

---
+++ Modell/load_data.py
@@ -0,0 +1,20 @@
+import numpy as np
+import huggingface_hub
+import os
+from datasets import load_dataset
+import pickle
+import pandas as pd
+
+state = os.environ('state')
+np.random.seed(state)
+
+huggingface_hub.login(token= os.environ['huggingface_token'])
+dataset = load_dataset("DS23-KI-Projekt/alzheimerdataset_split")
+
+df = dataset['train'].to_pandas()
+
+if not os.path.exists('dataset.pkl'):
+    os.makedirs('dataset.pkl')
+
+pd.to_pickle(df, "dataset.pkl") 
+
