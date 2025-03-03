e64846fd44e1f9412277c75ccb7abc3c5d6df6e5
Klagge, Philipp <philipp.klagge@wika.com>
2025-03-03 11:41:01 +0100

    fixing test

--- test/test_data_clean.py
+++ test/test_data_clean.py
@@ -9,12 +9,12 @@ def test_data_null():
     dataset = load_dataset("DS23-KI-Projekt/alzheimerdataset_split")
     df = dataset["train"].to_pandas()
     
-    assert df.isna().sum() == 0
+    assert df.isna().sum().sum() == 0
 
 def test_data_dublicate():
     login(token= os.environ['huggingface_token'])
     dataset = load_dataset("DS23-KI-Projekt/alzheimerdataset_split")
     df = dataset["train"].to_pandas()
-    assert len(df.index)-len(df.drop_duplicates().shape[0]) == 0
+    assert df.shape[0]-df.drop_duplicates().shape[0] == 0
 
 
